6f9302f8143488aa408f43564a4d167ebd056afe
Sangwoo Joh <lambdasnow@gmail.com>
2020-07-15 18:22:16 +0900

    Add testcases

---
+++ mlang/test/examples/test25.m
@@ -0,0 +1,15 @@
+let val foo = fn x => fn y =>
+  if x = y then x else y
+in
+  let val i = 1
+      val s = "hello world"
+      val b = true
+      val l = malloc 10
+  in
+    (
+      (foo i 2, foo s "bye world"),
+      (foo b false, foo (5,1) (1,2))
+    )
+  end
+end
+(* Result : Type checking fail -> pair compare *)
