7c45c5f8579c29a72e46a8fc5e75e815e78d63a1
Sangwoo Joh <lambdasnow@gmail.com>
2020-07-22 16:17:45 +0900

    Use Args module in toplevel entry

--- mlang/bin/toplevel.ml
+++ mlang/bin/toplevel.ml
@@ -1,30 +1,10 @@
 (** SNU 4190.310 Programming Languages *)
 
-open Mlang.M
-open Mlang.Pp
+open Cmdliner
+open Mlang
 
-let run () =
-  let print_m = ref false in
-  let src = ref "" in
-  let _ =
-    Arg.parse
-      [("-pp", Arg.Set print_m, "Print M program")]
-      (fun x -> src := x)
-      "Usage: ./run [<options>] <M file>"
-  in
-  let _ = Mlang.Error.init () in
-  let chan = if !src = "" then stdin else open_in !src in
-  let lexbuf = Lexing.from_channel chan in
-  let pgm = Mlang.Parser.program Mlang.Lexer.start lexbuf in
-  ( if !print_m then
-    let _ = print_string "== Input Program ==\n" in
-    let _ = M_Printer.print_exp pgm in
-    print_newline () ) ;
-  try M_Printer.print_typ (Mlang.Type_checker.check pgm) with
-  | TypeError _ ->
-      print_endline "Type Checking Failed"
-  | Failure e ->
-      print_endline e
+let driver () =
+  Term.(exit @@ eval_choice Args.default_cmd Args.cmds)
 
 
-let () = Printexc.catch run ()
+let () = driver ()
